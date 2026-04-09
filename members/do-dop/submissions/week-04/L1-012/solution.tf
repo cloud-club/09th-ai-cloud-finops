9ce76a86accb3d5c7dff54257c3925e892ac5863
박도연 <122397733+do-dop@users.noreply.github.com>
2026-04-09 18:30:47 +0900

    Upload solution.tf (do-dop, L1-012)

---
+++ members/do-dop/submissions/week-04/L1-012/solution.tf
@@ -0,0 +1,70 @@
+terraform {
+    required_providers {
+      aws = {
+        source  = "hashicorp/aws"
+        version = "~> 5.0"
+      }
+    }
+  }
+
+  provider "aws" {
+    region = "us-east-1"
+  }
+
+  resource "aws_s3_bucket" "s3-bucket-nb9qtp" {
+    bucket = "app-assets-prod"
+
+    tags = {
+      Name = "app-assets-prod"
+    }
+  }
+
+  resource "aws_s3_bucket_versioning" "s3-bucket-nb9qtp" {
+    bucket = aws_s3_bucket.s3-bucket-nb9qtp.id
+
+    versioning_configuration {
+      status = "Enabled"
+    }
+  }
+
+  resource "aws_s3_bucket_lifecycle_configuration" "s3-bucket-nb9qtp" {
+    bucket = aws_s3_bucket.s3-bucket-nb9qtp.id
+
+    rule {
+      id     = "expire-noncurrent-versions-30d"
+      status = "Enabled"
+
+      noncurrent_version_expiration {
+        noncurrent_days = 30
+      }
+    }
+  }
+
+  resource "aws_s3_bucket" "s3-bucket-c3hawv" {
+    bucket = "app-assets-staging"
+
+    tags = {
+      Name = "app-assets-staging"
+    }
+  }
+
+  resource "aws_s3_bucket_versioning" "s3-bucket-c3hawv" {
+    bucket = aws_s3_bucket.s3-bucket-c3hawv.id
+
+    versioning_configuration {
+      status = "Enabled"
+    }
+  }
+
+  resource "aws_s3_bucket_lifecycle_configuration" "s3-bucket-c3hawv" {
+    bucket = aws_s3_bucket.s3-bucket-c3hawv.id
+
+    rule {
+      id     = "expire-noncurrent-versions-30d"
+      status = "Enabled"
+
+      noncurrent_version_expiration {
+        noncurrent_days = 30
+      }
+    }
+  }
