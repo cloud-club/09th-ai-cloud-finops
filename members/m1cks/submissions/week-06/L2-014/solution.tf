0e8ac226233e99ce0aaf9388b6698e433abd417b
SeungJin In <sjin3568@gmail.com>
2026-04-23 19:23:24 +0900

    Upload solution.tf (m1cks, L2-014)

---
+++ members/m1cks/submissions/week-06/L2-014/solution.tf
@@ -0,0 +1,112 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_lambda_function" "lambda-function-oycs3i" {
+  function_name = "lambda-function-oycs3i"
+  role          = aws_iam_role.lambda-function-oycs3i_role.arn
+  handler       = "index.handler"
+  runtime       = "python3.11"
+
+  memory_size = 512
+  timeout     = 30
+
+  environment {
+    variables = {
+      ENVIRONMENT = "production"
+    }
+  }
+
+  tags = {
+    Name = "lambda-function-oycs3i"
+  }
+}
+
+resource "aws_lambda_function" "lambda-function-fl3urm" {
+  function_name = "lambda-function-fl3urm"
+  role          = aws_iam_role.lambda-function-fl3urm_role.arn
+  handler       = "index.handler"
+  runtime       = "python3.11"
+
+  memory_size = 512
+  timeout     = 30
+
+  environment {
+    variables = {
+      ENVIRONMENT = "production"
+    }
+  }
+
+  tags = {
+    Name = "lambda-function-fl3urm"
+  }
+}
+
+resource "aws_lambda_function" "lambda-function-63wtbt" {
+  function_name = "lambda-function-63wtbt"
+  role          = aws_iam_role.lambda-function-63wtbt_role.arn
+  handler       = "index.handler"
+  runtime       = "python3.11"
+
+  memory_size = 512
+  timeout     = 30
+
+  environment {
+    variables = {
+      ENVIRONMENT = "production"
+    }
+  }
+
+  tags = {
+    Name = "lambda-function-63wtbt"
+  }
+}
+
+resource "aws_lambda_function" "lambda-function-v17j3o" {
+  function_name = "lambda-function-v17j3o"
+  role          = aws_iam_role.lambda-function-v17j3o_role.arn
+  handler       = "index.handler"
+  runtime       = "python3.11"
+
+  memory_size = 512
+  timeout     = 30
+
+  environment {
+    variables = {
+      ENVIRONMENT = "production"
+    }
+  }
+
+  tags = {
+    Name = "lambda-function-v17j3o"
+  }
+}
+
+resource "aws_lambda_function" "lambda-function-jepj6c" {
+  function_name = "lambda-function-jepj6c"
+  role          = aws_iam_role.lambda-function-jepj6c_role.arn
+  handler       = "index.handler"
+  runtime       = "python3.11"
+
+  memory_size = 512
+  timeout     = 30
+
+  environment {
+    variables = {
+      ENVIRONMENT = "production"
+    }
+  }
+
+  tags = {
+    Name = "lambda-function-jepj6c"
+  }
+}
